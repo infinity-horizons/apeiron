94113722ce9182bbb4abb77c7bd7d6ad2f964d95
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-03-01 05:25:55 +0100

    None on "Add simple HTTP server"
    [ghstack-poisoned]

---
+++ infra/run.tf
@@ -0,0 +1,28 @@
+resource "google_cloud_run_service" "default" {
+  project  = var.project
+  name     = var.name
+  location = "europe-west9"
+
+  template {
+    spec {
+      containers {
+        image = "gcr.io/cloudrun/hello"
+      }
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [
+      template[0].spec[0].containers[0].image,
+      template[0].spec[0].containers[0].env
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_member" "default" {
+  location = google_cloud_run_service.default.location
+  project  = google_cloud_run_service.default.project
+  service  = google_cloud_run_service.default.name
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
