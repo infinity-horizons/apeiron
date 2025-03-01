a64ee471bccb6ff6c4c29d980147e57ccc0b1a6c
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-03-01 17:38:38 +0100

    Format infra

--- infra/dns.tf
+++ infra/dns.tf
@@ -1,9 +1,9 @@
 resource "cloudflare_dns_record" "default" {
-  zone_id  = var.zone
-  comment  = "Managed by Terraform"
-  content  = "ghs.googlehosted.com."
-  name     = var.name
-  proxied  = true
-  ttl      = 1
-  type     = "CNAME"
+  zone_id = var.zone
+  comment = "Managed by Terraform"
+  content = "ghs.googlehosted.com."
+  name    = var.name
+  proxied = true
+  ttl     = 1
+  type    = "CNAME"
 }
