09f447125a08d19d6ef7397de5460138195d881c
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-03-09 21:44:34 +0100

    Add persistent memory
    ghstack-source-id: 434684df431606375442b44a23359eabb87fa1bd
    Pull Request resolved: https://github.com/infinity-horizons/apeiron/pull/31

--- apeiron/agents/roast.py
+++ apeiron/agents/roast.py
@@ -4,7 +4,9 @@ from pathlib import Path
 
 from langchain_core.language_models.chat_models import BaseChatModel
 from langchain_core.tools.base import BaseTool
+from langgraph.checkpoint.memory import InMemorySaver
 from langgraph.prebuilt import create_react_agent
+from langgraph.store.memory import InMemoryStore
 
 from .utils import load_prompt
 
@@ -14,9 +16,14 @@ logger = logging.getLogger(__name__)
 def create_agent(tools: Sequence[BaseTool], model: BaseChatModel, **kwargs):
     """Create the roast generation node for the graph."""
     return create_react_agent(
+        name="Roast",
         model=model,
         tools=tools,
+        store=InMemoryStore(),
+        checkpointer=InMemorySaver(),
         prompt=load_prompt(
             Path(__file__).parent.resolve() / f"{Path(__file__).stem}.yaml",
         ),
+        version="v2",
+        **kwargs,
     )
