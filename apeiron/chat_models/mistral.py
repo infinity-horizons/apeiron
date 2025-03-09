dcc6fc102caea81ff48bfc0012855cd51e48ab22
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-03-09 14:15:58 +0000

    None on "Use Mistral official tokenizer"
    [ghstack-poisoned]

--- apeiron/chat_models/mistral.py
+++ apeiron/chat_models/mistral.py
@@ -1,12 +1,13 @@
 from langchain_mistralai.chat_models import ChatMistralAI
 from mistral_common.tokens.tokenizers.mistral import MistralTokenizer
 
-def get_token_ids(token: str) -> list[int]:
-    """Get the token ids from the tokenizer."""
-    tokenizer = MistralTokenizer.from_model("nemostral")
-    return tokenizer.encode(token).ids
-
 
 def get_model(model_name: str) -> ChatMistralAI:
     """Get the ChatMistralAI model."""
+    tokenizer = MistralTokenizer.from_model("nemostral")
+
+    def get_token_ids(token: str) -> list[int]:
+        """Get the token ids from the tokenizer."""
+        return tokenizer.encode(token).ids
+
     return ChatMistralAI(model_name=model_name, custom_get_token_ids=get_token_ids)
