3bc602f039436311a31abb3ad2c71006da9856fe
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-03-11 11:36:13 +0100

    Upstream secret management
    [ghstack-poisoned]

---
+++ infra/apeiron/install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# Fetch all outputs at once and save them to terraform.tfvars.json
+tofu -chdir="$(dirname "$0")/../apeiron-services" output -json |
+  jq 'with_entries(.value = .value.value)' >"$(dirname "$0")/terraform.tfvars.json"
+
+# Set secure permissions for the tfvars file
+chmod 600 "$(dirname "$0")/terraform.tfvars.json"
