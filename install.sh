d9cb46fc27b18075d3772b5df57fe45f9dbd7283
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-03-11 12:25:12 +0100

    Add dotenv generation
    ghstack-source-id: 57ca11c2bb7b43efc7e8627de6f7b33bc97cf865
    Pull Request resolved: https://github.com/infinity-horizons/apeiron/pull/51

--- install.sh
+++ install.sh
@@ -4,6 +4,14 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+# Create .env file from terraform outputs
+if [ -f "$(dirname "$0")/infra/apeiron/terraform.tfvars.json" ]; then
+  output=$(tofu -chdir="$(dirname "$0")/infra/apeiron-services" output -json)
+  echo "MISTRAL_API_KEY=$(jq -r '.apeiron.value.mistral_api_key' <<<"$output")" >"$(dirname "$0")/.env"
+  echo "DISCORD_TOKEN=$(jq -r '.apeiron.value.discord_token' <<<"$output")" >>"$(dirname "$0")/.env"
+  chmod 600 "$(dirname "$0")/.env"
+fi
+
 for dir in "$(dirname "$0")"/infra/*; do
   if [ -f "$dir/install.sh" ]; then
     bash "$dir/install.sh" 2>&1 |
