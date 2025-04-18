a7d33462df82bec400c3199fbc399c264ffaa386
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-04-19 01:02:11 +0200

    Update base for None on "Add Guild join information"
    [ghstack-poisoned]

--- infra/apeiron-services/output.tf
+++ infra/apeiron-services/output.tf
@@ -1,8 +1,9 @@
 output "apeiron" {
   value = {
-    google_ai_api_key = var.google_ai.api_key
-    mistral_api_key   = var.mistral.api_key
     discord_token     = var.discord.token
+    google_ai_api_key = var.google_ai.api_key
+    huggingface_token = var.huggingface.token
+    mistral_api_key   = var.mistral.api_key
   }
   sensitive = true
 }
