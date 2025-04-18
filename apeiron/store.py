27de66dc9f5b0adaaadea17b3c1d9ac92429d1a2
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-04-19 00:16:06 +0200

    Refactor memory handling
    [ghstack-poisoned]

---
+++ apeiron/store.py
@@ -0,0 +1,14 @@
+from langchain.embeddings import init_embeddings
+from langgraph.store.memory import InMemoryStore
+
+
+def create_store(model: str, **kwargs) -> InMemoryStore:
+    """Create a memory store."""
+
+    return InMemoryStore(
+        index={
+            "dims": 1536,
+            "embed": init_embeddings(model),
+            "fields": ["text"],
+        }
+    )
